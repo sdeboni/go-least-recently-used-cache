82a0139bd5353c076ed8d4749f79b7f81e48129e
Sam De Boni <sdeboni@gmail.com>
2025-04-29 09:53:58 -0400

    add LinkedList implementation for comparison

--- least_recently_used.go
+++ least_recently_used.go
@@ -1,24 +1,45 @@
 package lru
 
-func TestMe() string {
-  return "hello world"
-}
-
 type item struct {
   value interface{}
   index int
 }
 
-type Cache struct {
+type node struct {
+  value interface{}
+  key string
+  next *node
+  prev *node
+}
+type KV struct {
+   Key string
+   Value interface{}
+}
+
+type Cache interface {
+  Add(string, interface{})
+  Get(string) (interface{}, bool)
+  Dump() []KV
+} 
+
+type ArrayCache struct {
    data map[string]*item
    recent []string
 }
 
-func NewCache(capacity int) *Cache {
-  return &Cache{data: make(map[string]*item), recent: make([]string, 0, capacity)}
+type LinkedListCache struct {
+   data map[string]*node
+   first *node
+   last *node
+   capacity int
+   length int
 }
 
-func (cache *Cache) Add(key string, value interface{}) {
+func NewArrayCache(capacity int) Cache {
+  return &ArrayCache{data: make(map[string]*item), recent: make([]string, 0, capacity)}
+}
+
+func (cache *ArrayCache) Add(key string, value interface{}) {
   if _, ok := cache.Get(key); ok {
     cache.data[key].value = value
   } else {
@@ -33,7 +54,7 @@ func (cache *Cache) Add(key string, value interface{}) {
   }
 } 
 
-func (cache *Cache) Get(key string) (interface{}, bool) {
+func (cache *ArrayCache) Get(key string) (interface{}, bool) {
   if found, ok := cache.data[key]; ok {
     if found.index == len(cache.recent) - 1 {
        return found.value, true
@@ -51,4 +72,105 @@ func (cache *Cache) Get(key string) (interface{}, bool) {
   } else {
     return nil, false
   }
+}
+
+func (cache *ArrayCache) Dump() []KV {
+  if len(cache.recent) == 0 {
+    return []KV{}
+  }
+  
+  list := make([]KV, 0, len(cache.recent))
+
+  for _, key := range cache.recent {
+     if itm, ok := cache.data[key]; ok {
+       list = append(list, KV{key, itm.value})
+     } else {
+       panic("expected key not found")
+     }
+  }
+  return list
+}
+
+func NewLinkedListCache(capacity int) Cache {
+  return &LinkedListCache{data: make(map[string]*node), first: nil, last: nil, capacity: capacity, length: 0 }
+}
+
+func (cache *LinkedListCache) Add(key string, value interface{}) {
+  if found, ok := cache.data[key]; ok {
+    found.value = value 
+    cache.refresh(found)
+    return
+  }
+
+  newNode := node{value: value, key: key, prev: nil, next: nil}
+
+  if cache.length >= cache.capacity {
+    if cache.first == nil || cache.last == nil {
+      panic("linkedList cache with len > 0 has first or last == nil")
+    }
+
+    delete(cache.data, cache.first.key)
+
+    if cache.capacity == 1 {
+      cache.first = &newNode
+      cache.last = &newNode
+    } else {
+      cache.first.next.prev = nil
+      cache.first = cache.first.next
+      cache.last.next = &newNode
+      newNode.prev = cache.last
+      cache.last = &newNode
+    }
+  } else if cache.length == 0 {
+      cache.first = &newNode
+      cache.last = &newNode
+      cache.length = 1
+  } else {
+      cache.length++
+      cache.last.next = &newNode
+      newNode.prev = cache.last
+      cache.last = &newNode
+  }
+
+  cache.data[key] = &newNode
+}
+
+func (cache *LinkedListCache) Get(key string) (interface{}, bool) {
+  if found, ok := cache.data[key]; ok {
+    cache.refresh(found)
+    return found.value, true
+  } else {
+    return nil, false
+  }
+}
+
+func (cache *LinkedListCache) refresh(n *node) {
+  if cache.length < 2 || n == cache.last {
+    return
+  }
+  if n == cache.first {
+    cache.first.next.prev = nil
+    cache.first = cache.first.next
+  } else {
+    n.next.prev = n.prev
+    n.prev.next = n.next
+  }
+  n.prev = cache.last
+  n.next = nil
+  cache.last.next = n
+  cache.last = n
+}
+
+func (cache *LinkedListCache) Dump() []KV {
+  if cache.length == 0 {
+    return []KV{}
+  }
+
+  list := make([]KV, 0, cache.length)
+  node := cache.first
+  for node.next != nil {
+    list = append(list, KV{node.key, node.value})
+    node = node.next
+  }
+  return append(list, KV{node.key, node.value})
 }
--- least_recently_used_test.go
+++ least_recently_used_test.go
@@ -1,9 +1,17 @@
 package lru
 
-import "testing"
+import (
+  "testing"
+  "strconv"
+  "reflect"
+)
+
+func newCache(capacity int) Cache {
+  return NewLinkedListCache(capacity)
+}
 
 func TestBasicAdd(t *testing.T) {
-  cache := NewCache(5)
+  cache := newCache(5)
   cache.Add("one", 1)
   expected := 1
   actual, ok := cache.Get("one")
@@ -15,8 +23,64 @@ func TestBasicAdd(t *testing.T) {
   }
 }
 
+func TestGetRefreshedItem(t *testing.T) {
+  cache := newCache(2)
+  cache.Add("one", 1)
+  cache.Add("two", 2)
+  cache.Get("one")
+  cache.Add("three", 3)
+  if _, ok := cache.Get("one"); !ok {
+    t.Fatalf("key 'two' should not be found")
+  }
+  if _, ok := cache.Get("two"); ok {
+    t.Fatalf("key 'two' should not be found")
+  }
+  if _, ok := cache.Get("three"); !ok {
+    t.Fatalf("key 'three' should be found")
+  }
+}
+
+func TestDump(t *testing.T) {
+  cache := newCache(3)
+  cache.Add("one", 1)
+  cache.Add("two", 2)
+  cache.Add("three", 3)
+
+  expected := []KV {
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+  } 
+  actual := cache.Dump()
+  
+  if !reflect.DeepEqual(expected, actual) {
+    t.Fatalf("\nexpected: %v\ngot     : %v\n", expected, actual)
+  }
+}
+
+func TestDump2(t *testing.T) {
+  cache := newCache(3)
+  cache.Add("one", 1)
+  cache.Add("two", 2)
+  cache.Add("three", 3)
+  cache.Get("one")
+  cache.Add("four", 4)
+  cache.Get("three")
+
+  expected := []KV {
+    {"one", 1},
+    {"four", 4},
+    {"three", 3},
+  } 
+  actual := cache.Dump()
+  
+  if !reflect.DeepEqual(expected, actual) {
+    t.Fatalf("\nexpected: %v\ngot     : %v\n", expected, actual)
+  }
+}
+
 func TestBasicUpdate(t *testing.T) {
-  cache := NewCache(5)
+  cache := newCache(5)
   cache.Add("one", 1)
   cache.Add("one", "ONE")
   expected := "ONE"
@@ -30,7 +94,7 @@ func TestBasicUpdate(t *testing.T) {
 }
 
 func TestBumpOldItem(t *testing.T) {
-  cache := NewCache(2)
+  cache := newCache(2)
   cache.Add("one", 1)
   cache.Add("two", 2)
   cache.Add("three", 3)
@@ -41,5 +105,38 @@ func TestBumpOldItem(t *testing.T) {
   }
 }
 
+// Benchmark capacity: Array vs LinkedList 
+// About equal at 34K items, 
+// Array is about 2x faster for smaller capacities
+// LinkedList is progressively faster for higher capacities 
 
+const capacity = 34000
 
+func BenchmarkLruSliceImpl(b *testing.B) {
+  cache := NewArrayCache(capacity)
+  for n := 0; n < b.N; n++ {
+    doCacheOps(cache, capacity)
+  }
+}
+
+func BenchmarkLruLinkedListImpl(b *testing.B) {
+  cache := NewLinkedListCache(capacity)
+  for n := 0; n < b.N; n++ {
+    doCacheOps(cache, capacity) 
+  }
+}
+
+func doCacheOps(cache Cache, n int) {
+  for i := 0; i < n; i++ {
+    cache.Add(strconv.Itoa(i), i)
+  }
+  for i := n; i < 2*n; i++ {
+    cache.Add(strconv.Itoa(i), i)
+  }
+  for i := n; i < 2*n; i++ {
+    cache.Get(strconv.Itoa(i))
+  }
+  for i := n; i < 2*n; i++ {
+    cache.Add(strconv.Itoa(i), i+1)
+  }
+}
